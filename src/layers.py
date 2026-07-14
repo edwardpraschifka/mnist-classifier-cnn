643d8a6ad9ec8de9e195d61a06ef1df10e3456ea
epraschifka <edwardpraschifka@gmail.com>
2026-07-14 21:03:53 +0930

    Add: base Layer class

--- src/layers.py
+++ src/layers.py
@@ -1,1 +1,25 @@
 # Layer base class + ConvLayer, PoolLayer, ReLULayer, FlattenLayer, FCLayer
+
+from abc import ABC, abstractmethod
+import numpy as np
+
+class Layer(ABC):
+
+    @abstractmethod
+    def forward(self, X: np.ndarray):
+        """Return layer's output for input X"""
+
+        pass
+
+    @abstractmethod
+    def backward(self, dL_dOut):
+        """Return derivative of loss with respect to layer's
+            input, given derivative of loss with respect to
+            layer's output"""
+
+        pass
+    
+    def update(self, lr):
+        """Update layer's parameters, if it has any"""
+
+        pass
--- src/test_layers.py
+++ src/test_layers.py
@@ -1,1 +1,11 @@
 # per-layer gradient checks against PyTorch
+
+import pytest
+import numpy as np
+
+from layers import Layer
+
+class TestBaseLayer:
+    def test_instantiate(self):
+        with pytest.raises(TypeError):
+            my_layer_class = Layer()
