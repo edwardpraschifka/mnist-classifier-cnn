87bdf7735c143791c4e957959124fa2cc8121d82
epraschifka <edwardpraschifka@gmail.com>
2026-07-16 12:57:49 +0930

    Add: utils.convolve function
    To be used in ConvLayer

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from utils import convolve_1c
+from utils import convolve_1c, convolve
 
 def test_conv_1c():
     X = np.array([
@@ -22,3 +22,33 @@ def test_conv_1c():
 
     assert np.array_equal(Y, Y_actual)
     
+
+def test_conv():
+    X = np.zeros((2,3,3))
+    X[0] = [[1,0,1],
+           [-1,1,0],
+           [-1,-1,1]]
+    
+    X[1] = [[0,0,1],
+           [1,-1,1],
+           [1,1,0]]
+
+    W = np.zeros((3,2,2,2))
+
+    W[0] = [[[1,2], [3,4]], [[4,3], [2,1]]]
+    W[1] = [[[1,3], [2,4]], [[3,2], [1,4]]]
+    W[2] = [[[2,1], [3,4]], [[1,2], [4,3]]]
+
+    Y = convolve(W,X)
+    Y_actual = np.zeros((3,2,2))
+
+    Y_actual[0] = [[ 3,  7],
+            [-2,  3]]
+
+    Y_actual[1] = [[ 0, 10],
+            [ 2,  3]]
+
+    Y_actual[2] = [[ 4,  5],
+            [-2,  8]]
+    
+    assert np.array_equal(Y, Y_actual)
--- src/utils.py
+++ src/utils.py
@@ -14,3 +14,18 @@ def convolve_1c(K: np.ndarray,X: np.ndarray):
             Y[i][j] = np.sum(K * X[i:i+kh, j:j+kw])
     
     return Y
+
+def convolve(W: np.ndarray, X: np.ndarray):
+    """Performs a convolution over multiple channels"""
+
+    wh, ww, kh, kw = np.shape(W)
+    xc, xh, xw = np.shape(X)
+    yc, yh, yw = wh, xh - kh + 1, xw - kw + 1
+    Y = np.zeros((yc, yh, yw))
+
+    for i in range(wh):
+        for j in range(ww):
+            Y[i] += convolve_1c(W[i][j], X[j])
+
+    return Y
+
